e48dd159c6eb8e37fc7eddec0ec5a56b50146be0
Sayana <nisayana23@yandex.ru>
2020-07-18 22:18:24 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,9 +1,11 @@
 def find_element_index(array, value_to_find)
-  counter = 0
-  while counter < array.length do
-    if array[counter] == value_to_find
-      return count
-    end
-    counter+=1
-  end
+  # counter = 0
+  # while counter < array.length do
+  #   if array[counter] == value_to_find
+  #     return count
+  #   end
+  #   counter+=1
+  # end
+
+  array.index(value_to_find)
 end
